76b9a8d2b64d683102a1f92fa0cec3479c38c2fd
royzhao123 <98574256+royzhao123@users.noreply.github.com>
2022-01-27 22:03:40 -0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Roy Zhao, hzhao123\nJenny Rong, rongt2\nLingji Kong, lingjk2" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
