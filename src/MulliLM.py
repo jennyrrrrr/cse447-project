81ea95bafa4b59624569b3966ca0741b31acccf6
jennyrrrrr <49295374+jennyrrrrr@users.noreply.github.com>
2022-02-15 21:02:39 -0800

    update j

---
+++ src/MulliLM.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+import numpy as np
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as Data
+import tqdm
+
+n_hidden = 256
+SEQUENCE_LENGTH = 100
+BATCH_SIZE = 256
+FEATURE_SIZE = 512
+TEST_BATCH_SIZE = 256
+EPOCHS = 20
+LEARNING_RATE = 0.003
+WEIGHT_DECAY = 0.0005
+PRINT_INTERVAL = 10
+#file_path = BASE_PATH
+file_path = '/content'
+batch_size = 256
+vocab_size = len(word2idx)
+
+class MultiLM(nn.Module):
+    def __init__(self):
+        super(MultiLM, self).__init__()
+        self.vocab_size = vocab_size
+        self.feature_size = FEATURE_SIZE
+        self.encoder = nn.Embedding(self.vocab_size, self.feature_size, padding_idx = 0)
+        self.rnn = nn.RNN(self.feature_size, hidden_size=n_hidden)
+        self.layer_out = nn.Linear(n_hidden, self.vocab_size)
+
+    def forward(self, x, hidden):
+        x = x.transpose(0,1)
+        x = self.encoder(x)
+        x, hidden = self.rnn(x, hidden)
+        x = x[-1]
+        x = self.layer_out(x)
+        return x
+
+    def loss(self):
+        loss = nn.CrossEntropyLoss()
+        return loss
+
+    def save_model(self, file_path):
+        torch.save(self.state_dict(), file_path)
+
+    def load_model(self, file_path):
+        torch.load(self, file_path)
--- src/myprogram.py
+++ src/myprogram.py
@@ -21,75 +21,145 @@ from nltk import word_tokenize
 
 import Trie
 
-class MyModel:
-    """
-    This is a starter model to get you started. Feel free to modify this file.
-    """
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+import numpy as np
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as Data
+import MultiLM
 
-    @classmethod
-    def load_training_data(cls):
-        # your code here
-        # this particular model doesn't train
-        train_df = dataset['train']
-        val_df = dataset['validation']
-        test_df = dataset['test']
-        corpus = train_df["review_body"] + val_df["review_body"] + test_df["review_body"]
-        vocab = []
-        tokenized_corpus = []
-        for sentence in corpus:
-            tok_sen = word_tokenize(sentence.lower())
-            tokenized_corpus.append(tok_sen)
-            for word in tok_sen:
-                if word not in vocab:
-                    vocab.append(word)
-        word2idx = {w: idx + 1 for (idx, w) in enumerate(vocab)}
-        word2idx['<pad>'] = 0
-        idx2word = {idx + 1: w for (idx, w) in enumerate(vocab)}
-        idx2word[0] = '<pad>'
+n_hidden = 256
+SEQUENCE_LENGTH = 100
+BATCH_SIZE = 256
+FEATURE_SIZE = 512
+TEST_BATCH_SIZE = 256
+EPOCHS = 20
+LEARNING_RATE = 0.003
+WEIGHT_DECAY = 0.0005
+PRINT_INTERVAL = 10
 
-    @classmethod
-    def load_test_data(cls, fname):
-        # your code here
-        data = []
-        with open(fname) as f:
-            for line in f:
-                inp = line[:-1]  # the last character is a newline
-                data.append(inp)
-        return data
+word2idx = []
+tokenized_corpus = []
+vocab_size = 0
 
-    @classmethod
-    def write_pred(cls, preds, fname):
-        with open(fname, 'wt') as f:
-            for p in preds:
-                f.write('{}\n'.format(p))
+def load_training_data(cls):
+    train_df = dataset['train']
+    val_df = dataset['validation']
+    test_df = dataset['test']
+    corpus = train_df["review_body"] + val_df["review_body"] + test_df["review_body"]
 
-    def run_train(self, data, work_dir):
-        # your code here
-        pass
+    vocab = []
+    token_set = set()
+    tokenized_corpus = []
+    i = 0
+    for sentence in corpus:
+        if i % 10000 == 0: print(i)
+        tok_sen = word_tokenize(sentence.lower())
+        tokenized_corpus.append(tok_sen)
+        token_set.update(tok_sen)
+        i += 1
+    #vocab = np.hstack(tokenized_corpus)
+    #vocab = np.unique(vocab)
+    vocab = list(token_set)
+    word2idx = {w: idx+1 for (idx, w) in enumerate(vocab)}
+    word2idx['<pad>'] = 0
+    idx2word = {idx+1 : w for (idx, w) in enumerate(vocab)}
+    idx2word[0] = '<pad>'
 
-    def run_pred(self, data):
-        # your code here
-        preds = []
-        all_chars = string.ascii_letters
-        for inp in data:
-            # this model just predicts a random character each time
-            top_guesses = [random.choice(all_chars) for _ in range(3)]
-            preds.append(''.join(top_guesses))
-        return preds
+vocab_size = len(word2idx)
 
-    def save(self, work_dir):
-        # your code here
-        # this particular model has nothing to save, but for demonstration purposes we will save a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
-            f.write('dummy save')
+def one_hot(k): 
+    v = np.zeros(len(word2idx))
+    v[k] = 1
+    return v
+    
+def data_helper():
+    n_rows = sum([len(sen) for sen in tokenized_corpus]) - len(tokenized_corpus)
+    vectorized_sents = [[word2idx[tok] for tok in sent if tok in word2idx] for sent in tokenized_corpus]
+    sent_tensor = torch.zeros((n_rows, 4)).int()
+    labels = []
+    k = 0
 
-    @classmethod
-    def load(cls, work_dir):
-        # your code here
-        # this particular model has nothing to load, but for demonstration purposes we will load a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-            dummy_save = f.read()
-        return MyModel()
+    for sen in vectorized_sents:
+        for i in range(len(sen)-1):
+            if i == 0:
+                sent_tensor[k][0] = sen[i]
+            elif i == 1:
+                sent_tensor[k][0] = sen[i-1]
+                sent_tensor[k][1] = sen[i]
+            elif i == 2:
+                sent_tensor[k][0] = sen[i-2]
+                sent_tensor[k][1] = sen[i-1]
+                sent_tensor[k][2] = sen[i]
+            else:
+                sent_tensor[k][0] = sen[i-2]
+                sent_tensor[k][1] = sen[i-2]
+                sent_tensor[k][2] = sen[i-1]
+                sent_tensor[k][3] = sen[i]
+            labels.append(sen[i+1])
+            k += 1
+    labels = torch.tensor(labels)
+
+    return sent_tensor, labels
+
+def load_test_data(fname):
+    # your code here
+    data = []
+    with open(fname) as f:
+        for line in f:
+            inp = line[:-1]  # the last character is a newline
+            data.append(inp)
+    return data
+
+def write_pred(preds, fname):
+    with open(fname, 'wt') as f:
+        for p in preds:
+            f.write('{}\n'.format(p))
+
+def run_train(model, device, optimizer, loader, lr, epoch, log_interval):
+    model.train()
+    losses = []
+    for batch_idx, (sent_tensor, labels) in enumerate(loader):
+        data, label = sent_tensor.to(device), labels.to(device)
+        hidden = torch.zeros(1, data.shape[0], n_hidden).to(device)
+        pred = model(data, hidden)
+        optimizer.zero_grad()
+        loss = model.loss()
+        loss = loss(pred, label)
+        losses.append(loss.item())
+        loss.backward()
+        optimizer.step()
+    avg_loss = sum(losses) / len(losses)
+    print('Average Loss', avg_loss)
+    return avg_loss
+
+def run_pred(data):
+    # your code here
+    preds = []
+    all_chars = string.ascii_letters
+    for inp in data:
+        # this model just predicts a random character each time
+        top_guesses = [random.choice(all_chars) for _ in range(3)]
+        preds.append(''.join(top_guesses))
+    return preds
+
+'''
+def save(work_dir):
+    # your code here
+    # this particular model has nothing to save, but for demonstration purposes we will save a blank file
+    with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
+        f.write('dummy save')
+
+def load(work_dir):
+    # your code here
+    # this particular model has nothing to load, but for demonstration purposes we will load a blank file
+    with open(os.path.join(work_dir, 'model.checkpoint')) as f:
+        dummy_save = f.read()
+    return MyModel()
+'''
 
 
 if __name__ == '__main__':
@@ -102,23 +172,31 @@ if __name__ == '__main__':
 
     random.seed(0)
 
+    file_path = args.work_dir
+
     if args.mode == 'train':
         if not os.path.isdir(args.work_dir):
             print('Making working directory {}'.format(args.work_dir))
             os.makedirs(args.work_dir)
         print('Instatiating model')
-        model = MyModel()
+        device = torch.device("cuda")
+        model = MultiLM().to(device)
+        optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
         print('Loading training data')
-        train_data = MyModel.load_training_data()
+        sent_tensor, labels = data_helper()
+        dataset = Data.TensorDataset(sent_tensor, labels)
+        loader = Data.DataLoader(dataset, BATCH_SIZE, True)        
         print('Training')
-        model.run_train(train_data, args.work_dir)
-        print('Saving model')
-        model.save(args.work_dir)
+        for epoch in range(EPOCHS):
+            print("Epoch :", epoch)
+            avg_loss = run_train(model, device, optimizer, loader, LEARNING_RATE, epoch, PRINT_INTERVAL)
+            print('Saving model')
+            model.save_model(args.work_dir + 'model.checkpoint')
     elif args.mode == 'test':
         print('Loading model')
-        model = MyModel.load(args.work_dir)
+        model = MultiLM.load_model(args.work_dir)
         print('Loading test data from {}'.format(args.test_data))
-        test_data = MyModel.load_test_data(args.test_data)
+        test_data = MultiLM.load_test_data(args.test_data)
         print('Making predictions')
         pred = model.run_pred(test_data)
         print('Writing predictions to {}'.format(args.test_output))
